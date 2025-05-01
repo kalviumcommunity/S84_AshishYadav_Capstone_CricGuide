cfd11a1bec4b8d0b60f04d77f122171f18864ed6
Ashish <iashishyadav17@gmai.com>
2025-05-01 15:27:31 +0530

    Get request done

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,6 +6,16 @@ const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 
+router.get('/users', protect, async (req, res) => {
+    try {
+      const users = await User.find().select('-password');
+      res.status(200).json(users);
+    } catch (err) {
+      console.error('Error fetching users:', err.message);
+      res.status(500).json({ message: 'Server Error' });
+    }
+});
+  
 
 router.post('/signup', register);
 router.post('/login', login);
