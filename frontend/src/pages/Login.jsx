b23c4616d3252350b41e9f297a97a5eb95af0083
Ashish <iashishyadav17@gmai.com>
2025-07-10 18:39:32 +0530

    updated

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("http://s84-ashishyadav-capstone-cricguide-1.onrender.com/api/auth/login", {
         email,
         password
       });
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -13,7 +13,7 @@ const Signup = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/register", {
+      const res = await axios.post("https://s84-ashishyadav-capstone-cricguide-1.onrender.com/api/auth/register", {
         name,
         email,
         password
